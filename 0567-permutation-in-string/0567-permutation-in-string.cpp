d20cd726d983a07c67b217acc461084c6e82c87b
asfina9591-u <asfina9591@gmail.com>
2026-07-12 18:57:03 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.13%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,41 @@
+// this code is similar to find all Anagrams in a string
+
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        if (n > m)
+            return false;
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+
+        // Frequency of s1 and first window of s2
+        for (int i = 0; i < n; i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        // Check first window
+        if (freq1 == freq2)
+            return true;
+
+        // Slide the window
+        for (int i = n; i < m; i++) {
+
+            // Add new character
+            freq2[s2[i] - 'a']++;
+
+            // Remove old character
+            freq2[s2[i - n] - 'a']--;
+
+            // Compare frequencies
+            if (freq1 == freq2)
+                return true;
+        }
+
+        return false;
+    }
+};
