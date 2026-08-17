d615e1e4f95df51f0396d2ba6993ef17f1318b17
asfina9591-u <asfina9591@gmail.com>
2026-08-17 19:55:14 +0530

    Time: 196 ms (70.13%), Space: 10.3 MB (99.46%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
 
-    bool dfs(vector<vector<char>>& board, string& word,
-             int r, int c, int index) {
+   bool dfs(vector<vector<char>>&board, string& word, int r, int c, int index){
 
-        // Finished the whole word
-        if (index == word.length())
+    if(index==word.length())
+    return true;
+    // Invalid cell
+    if(r<0 || r>=board.size() ||
+       c<0 || c>=board[0].size() ||
+       board[r][c]!=word[index])
+       return false;
+
+    // save the current character
+    char temp=board[r][c];
+    board[r][c]='#';
+    //try for the 4 diections
+    bool found= dfs(board, word, r+1, c, index+1) ||
+                dfs(board, word, r-1, c, index+1) ||
+                dfs(board, word, r, c+1, index+1) ||
+                dfs(board, word, r, c-1, index+1);
+
+    board[r][c]=temp;
+    return found;
+   }
+   bool exist(vector<vector<char>>&board, string& word){
+    for(int i=0;i<board.size();i++){
+        for(int j=0;j<board[0].size();j++){
+            if(dfs(board, word, i, j, 0))
             return true;
-
-        // Invalid cell
-        if (r < 0 || r >= board.size() ||
-            c < 0 || c >= board[0].size() ||
-            board[r][c] != word[index])
-            return false;
-
-        // Save current character
-        char temp = board[r][c];
-
-        // Mark cell as used
-        board[r][c] = '#';
-
-        // Try 4 directions
-        bool found = dfs(board, word, r + 1, c, index + 1) ||
-                     dfs(board, word, r - 1, c, index + 1) ||
-                     dfs(board, word, r, c + 1, index + 1) ||
-                     dfs(board, word, r, c - 1, index + 1);
-
-        // Undo / backtrack
-        board[r][c] = temp;
-
-        return found;
-    }
-
-    bool exist(vector<vector<char>>& board, string word) {
-
-        // Try every cell as starting point
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-
-                if (dfs(board, word, i, j, 0))
-                    return true;
-            }
         }
-
-        return false;
     }
+        return false;
+    
+   }
 };
+
+
+
+
+    
+
+   
